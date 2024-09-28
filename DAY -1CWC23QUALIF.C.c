b5de8ecb65d00eefa0fe02fe718c6bcbe90d164d
RAJSENGAR9340 <surusengar0905@gmail.com>
2024-09-28 23:22:57 +0530

    Add files via upload

---
+++ DAY -1CWC23QUALIF.C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int x;
+	printf("enter the value of score ");
+	scanf("%d",&x);
+	if(x>=12)
+	printf("team is quailifed for next stage ");
+	else
+	printf("team is not  quailifed for next stage ");
+	return 0;
+}
+
